2cfde76ad394c91ea0de48d5abe4d87f3cb2fbde
shuye <78575544+shuye73@users.noreply.github.com>
2021-02-27 00:28:30 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -124,4 +124,4 @@
 # 京东疯狂的JOY挂机
 5 5 */2 * *  node /scripts/jd_crazy_joy_coin.js >> /scripts/logs/jd_crazy_joy_coin.log 2>&1
 # 京东摇一摇
-0 1,9,17 * * *   node /scripts/jd_shake.js >> /scripts/logs/jd_shake.log 2>&1
+0 1,17 * * *   node /scripts/jd_shake.js >> /scripts/logs/jd_shake.log 2>&1
