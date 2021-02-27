cbf06391683e59552e29b93eead7dea02c2627a7
shuye <78575544+shuye73@users.noreply.github.com>
2021-02-27 18:01:18 +0800

    fix

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -28,7 +28,7 @@ const $ = new Env('京豆变动通知');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let cookiesData = $.getdata('CookiesJD') || "[]";
+let allMessage = '';
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '';
