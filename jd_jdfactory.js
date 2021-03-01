56b3ce6d9fecd83ebc43dfc941882f6d8324a125
shuye <78575544+shuye73@users.noreply.github.com>
2021-03-02 01:14:00 +0800

    fix

--- jd_jdfactory.js
+++ jd_jdfactory.js
@@ -36,7 +36,7 @@ const notify = $.isNode() ? require('./sendNotify') : '';
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 let jdNotify = true;//是否关闭通知，false打开通知推送，true关闭通知推送
 const randomCount = $.isNode() ? 20 : 5;
-let helpAuther = true;
+let helpAuthor = true;
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 if ($.isNode()) {
